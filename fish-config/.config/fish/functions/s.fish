84aa1c730eababb51f72ab51be31396097cb19a1
sergio.soares <acad.sergio@gmail.com>
2022-03-27 19:24:14 -0300

    Script for custom configurations

--- fish-config/.config/fish/functions/s.fish
+++ fish-config/.config/fish/functions/s.fish
@@ -1,4 +1,8 @@
 # Defined in - @ line 1
 function s
-    cat ~/.ssh/config | grep 'Host ' | sed -e 's/Host /ssh /' | fzf $argv | xclip -selection clipboard
+    # cat ~/.ssh/config | grep 'Host ' | sed -e 's/Host /ssh /' | fzf $argv | xclip -selection clipboard
+    set options (cat ~/.ssh/config | grep 'Host ' | sed -e 's/Host /ssh /' | fzf) 
+    
+    echo $options
+    eval $options
 end
